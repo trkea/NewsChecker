5aec048f3a1375c0f5f259cbd179961bfcb6e148
trkea <trs1141@yahoo.co.jp>
2018-09-04 22:23:53 +0900

    add GunosyNews

--- src/GunosyNews.py
+++ src/GunosyNews.py
@@ -12,3 +12,21 @@ class GunosyNews():
 		soup = BeautifulSoup(html,"html.parser")
 		return soup
 
+	def get_news(self):
+		html = self.get_html()
+		div_news = html.find_all("div",class_="list_title")
+		all_news = []
+		i = 0
+		for div in div_news:
+			a_tag = div.find("a")
+			text = a_tag.text
+			href = a_tag.get('href')
+			news_info ={"title":text, "href":href}
+			all_news.append(news_info)
+		return all_news
+
+
+
+
+
+
