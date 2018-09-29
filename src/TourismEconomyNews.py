ef37d36417bbf61455b37f7a859eb4846af1b28e
trkea <trs1141@yahoo.co.jp>
2018-09-29 11:12:30 +0900

    fixed TourismEconomyNews continue

--- src/TourismEconomyNews.py
+++ src/TourismEconomyNews.py
@@ -1,6 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from src import  HtmlParser as hp
+import  HtmlParser as hp
 
 class TourismEconomyNews():
     def __init__(self,url):
@@ -15,16 +15,19 @@ class TourismEconomyNews():
         business = [16,17,18,19]
         tourism = [20,21,22,23]
         inbound = [24,25,26,27]
-        topic = [39,40,41,42]
+        topic = [40,41,42,43]
         all_news = []
         for div_tag,ul_tag in zip(div_news,ul_times):
+            if not(i in travel or i in business or i in tourism or  i in inbound or i in topic):
+                i += 1
+                continue
             title = div_tag.find("a").text
+            if title.find("【PR】") != -1:
+                i += 1
+                continue  
             href = div_tag.find("a").get("href")
             time = ul_tag.find("li").text
-            if title.find("【PR】") != -1:
-                continue  
-            if i in travel or i in business or i in tourism or  i in inbound or i in topic:
-                news_info = {"title": title, "href": href, "time": time}
-                all_news.append(news_info)
+            news_info = {"title": title, "href": href, "time": time}
+            all_news.append(news_info)
             i += 1      
         return all_news
